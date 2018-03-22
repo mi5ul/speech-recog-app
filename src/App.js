f5aeb687cfaba4673b16f46720800a5ce8e0c3db
mi5ul <mirt.suljic@gmail.com>
2018-03-23 09:41:58 +1300

    Added Font Awesome

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import NavBar from './Components/NavBar/NavBar';
 import Background from './Components/Background/Background';
 import SpeechRecognizer from './Components/SpeechRecognizer/SpeechRecognizer';
 
+import fontawesome from '@fortawesome/fontawesome'
+import { faMicrophone, faMicrophoneSlash } from '@fortawesome/fontawesome-free-solid'
+ 
+fontawesome.library.add(faMicrophone, faMicrophoneSlash)
+
 class App extends Component {
   render() {
     return (
--- src/Components/SpeechRecognizer/ButtonPanel.js
+++ src/Components/SpeechRecognizer/ButtonPanel.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import './ButtonPanel.css'
 
 const ButtonPanel = ({ listenOnClick, translateOnClick, stopRecordingOnClick, disableTranslate }) => {
 	return (
 		<div>
-			<button onClick={listenOnClick}>Record</button>
-      <button onClick={stopRecordingOnClick}>Stop</button>
+			<button onClick={listenOnClick}><FontAwesomeIcon icon="microphone" /></button>
+      <button onClick={stopRecordingOnClick}><FontAwesomeIcon icon="microphone-slash" /></button>
       <button onClick={translateOnClick} disabled={disableTranslate}>Translate</button>
 		</div>
 	);
